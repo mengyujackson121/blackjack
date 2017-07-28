5996f7d2142f5e456e7fcb658217597deca1283d
mengyujackson121 <mengyujackson121@gmail.com>
2017-07-28 16:21:41 -0400

    Add files via upload

--- blackjack.py
+++ blackjack.py
@@ -1,19 +1,23 @@
-# one deck, new deck each new game
 import random
-
 
 rank_only = False
 stop_game = False
+money = 100
 
-def winner(player, dealer):
-    if cal_value(player) > cal_value(dealer):
+
+def winner(play, deal):
+    if cal_value(play) > cal_value(deal):
         print("Player Win!")
+        return True
 
-    elif cal_value(player) == cal_value(dealer):
+    elif cal_value(play) == cal_value(deal):
         print("Tie Dealer Win!")
+        return False
 
     else:
         print("Dealer Win!")
+        return False
+
 
 def cal_value(hand):
     num_aces = 0
@@ -55,18 +59,30 @@ def cal_value(hand):
     return value
 
 
-def get_card(role,card):
+def get_card(role, card):
     role.append(card.pop(random.randrange(len(card))))
 
-def result(player, dealer):
-    print("Player: ", player, cal_value(player), 'Value: ', cal_value(player))
-    print("Dealer: ", dealer, cal_value(dealer), 'Value: ', cal_value(dealer))
+
+def result(player_, dealer_):
+    print("Player: ", player, cal_value(player), 'Value: ', cal_value(player_))
+    print("Dealer: ", dealer, cal_value(dealer), 'Value: ', cal_value(dealer_))
+
+
+def down(money_):
+    val = int(input("How much down? (1 - " + str(money_) + "): "))
+    if 0 >= val or val > money_:
+        print("Error: You don't have that much Money!")
+        return down(money_)
+    return val
+
 
 while input("Wanna Start a New Game? (q for quit, any key contiune...)") != 'q':
-    game_over = False
-
+    put_down = down(money)
+    print(put_down)
+    win = False
+    bj = False
     if input('Rank or Not? (r to Rank only: )') == 'r':
-        card_deck = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]*4
+        card_deck = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"] * 4
         rank_only = True
 
     else:
@@ -85,34 +101,51 @@ while input("Wanna Start a New Game? (q for quit, any key contiune...)") != 'q':
         print("Player: ", player, 'Value: ', cal_value(player))
         dealer_hand_show = [dealer[0]]
         print("Dealer: ", dealer_hand_show, 'Value: ', cal_value(dealer_hand_show))
-
+        if cal_value(dealer) == 21:
+            print("Dealer Win!")
+            result(player, dealer)
+        elif cal_value(player) == 21 and cal_value(dealer) != 21:
+            print("BlackJack!")
+            bj = True
+            win = True
+            result(player, dealer)
 
     while input('Hit or Stay? (h to hit, any else for stay: )') == 'h':
         get_card(player, card_deck)
         print(player, 'Value: ', cal_value(player))
-        if cal_value(player) > 21:
-            game_over = True
+        if cal_value(player) == 21:
+            print("Player Win! 21 point!")
+            win = True
+            result(player, dealer)
+            break
+        elif cal_value(player) > 21:
+            result(player, dealer)
             break
 
     while cal_value(dealer) < 17:
         get_card(dealer, card_deck)
         if cal_value(dealer) > 21:
-            game_over = True
+            win = True
             break
 
     if cal_value(player) <= 21 and cal_value(dealer) <= 21:
-        result(player,dealer)
-        winner(player, dealer)
-        game_over = True
+        result(player, dealer)
+        win = winner(player, dealer)
 
-    elif cal_value(player) >21:
+    elif cal_value(player) > 21:
         result(player, dealer)
         print("Player Lose! Over 21!")
 
-
     else:
-        result(player,dealer)
+        result(player, dealer)
         print("Dealer Lose! Over 21!")
 
-
-
+    if win is True and bj is True:
+        money = money + put_down * 2
+    elif win is True:
+        money = money + put_down
+    else:
+        money = money - put_down
+    print("total :", money)
+    if money <= 0:
+        quit()
