03fc3c590aa392cf111b220ec2e82a8d82926481
mengyujackson121 <mengyujackson121@gmail.com>
2017-08-01 22:46:25 -0400

    fix errors after function refactor

--- blackjack.py
+++ blackjack.py
@@ -6,17 +6,17 @@ Hand = List[card.Card]
 
 def main():
     rank_only = False
-    stop_game = False
     money = 100
+    shift = 0
 
-    start_game(money)
-    get_player_size()
-    get_start_hands(card_deck=)
-    check_win_at_begining(dealer=, player=)
-    player_turn(player=,dealer=,card_deck=,rank_only)
-    dealer_turn(dealer=,card_deck=)
-    check_winner(player=,dealer=)
-    give_money(win, bj, money,put_down=)
+    card_deck, put_down = start_game(money)
+    rank_only = choice_rank()
+    # shift = get_player_size(shift)
+    player, dealer = get_start_hands(card_deck)
+    player_turn(player, dealer, card_deck, rank_only)
+    dealer_turn(dealer, card_deck)
+    win = check_winner(player, dealer, rank_only)
+    money = give_money(win, money, put_down, player)
     is_enough_money(money)
 
 
@@ -63,13 +63,13 @@ def cal_value(hand: Hand):
     return value
 
 
-def get_card(role, card):
-    role.append(card.pop(random.randrange(len(card))))
+def get_card(role, card_deck):
+    role.append(card_deck.pop(random.randrange(len(card_deck))))
 
 
-def result(player_, dealer_):
-    print("Player: ", display_list(player_), cal_value(player_), 'Value: ', cal_value(player_))
-    print("Dealer: ", display_list(dealer_), cal_value(dealer_), 'Value: ', cal_value(dealer_))
+def result(player_, dealer_, rank_only):
+    print("Player: ", display_list(player_, rank_only), cal_value(player_), 'Value: ', cal_value(player_))
+    print("Dealer: ", display_list(dealer_, rank_only), cal_value(dealer_), 'Value: ', cal_value(dealer_))
 
 
 def down(money_):
@@ -84,91 +84,82 @@ def start_game(money):
     while input("Wanna Start a New Game? (q for quit, any key contiune...)") != 'q':
         put_down = down(money)
         print(put_down)
-        win = False
-        bj = False
         card_deck = card.standard_deck()
-        if input('Rank or Not? (r to Rank only: )') == 'r':
-            rank_only = True
-        else:
-            rank_only = False
+        return card_deck, put_down
 
 
-def get_player_size():
-    shift = 0
+def choice_rank():
+    if input('Rank or Not? (r to Rank only: )') == 'r':
+        rank_only = True
+    else:
+        rank_only = False
+    return rank_only
+
+
+def get_player_size(shift):
     while 1 > shift or 5 < shift:
         try:
             shift = int(input("Please enter your players (1 - 5) : "))
+            return shift
         except ValueError:
-            print("That wasn't an integer :(")
+            get_player_size(shift)
 
 
 def get_start_hands(card_deck):
-    player = []
     dealer = []
+    player = []
     while len(dealer) < 2:
         get_card(player, card_deck)
         get_card(dealer, card_deck)
+    return player, dealer
 
 
-def check_win_at_begining(dealer,player):
-    if len(dealer) == 2 and len(player) == 2:
-        print("Player: ", display_list(player), 'Value: ', cal_value(player))
-        dealer_hand_show = [dealer[0]]
-        print("Dealer: ", display_list(dealer_hand_show), 'Value: ', cal_value(dealer_hand_show))
-        if cal_value(dealer) == 21:
-            print("Dealer Win!")
-            result(player, dealer)
-        elif cal_value(player) == 21 and cal_value(dealer) != 21:
-            print("BlackJack!")
-            bj = True
-            win = True
-            result(player, dealer)
-
-
-def player_turn(player, dealer, card_deck,rank_only):
+def player_turn(player, dealer, card_deck, rank_only):
     while input('Hit or Stay? (h to hit, any else for stay: )') == 'h':
         get_card(player, card_deck)
-        print(display_list(player,rank_only), 'Value: ', cal_value(player))
+        print(display_list(player, rank_only), 'Value: ', cal_value(player))
         if cal_value(player) == 21:
             print("Player Win! 21 point!")
-            win = True
-            result(player, dealer)
-            break
+            result(player, dealer, rank_only)
+            return
+
         elif cal_value(player) > 21:
-            result(player, dealer)
-            break
+            result(player, dealer, rank_only)
+            return
 
 
-def dealer_turn(dealer,card_deck):
+def dealer_turn(dealer, card_deck):
     while cal_value(dealer) < 17:
         get_card(dealer, card_deck)
         if cal_value(dealer) > 21:
-            win = True
-            break
+            return
 
 
-def check_winner(player, dealer):
+def check_winner(player, dealer, rank_only):
     if cal_value(player) <= 21 and cal_value(dealer) <= 21:
-        result(player, dealer)
-        win = winner(player, dealer)
+        result(player, dealer, rank_only)
+        return winner(player, dealer)
 
     elif cal_value(player) > 21:
-        result(player, dealer)
+        result(player, dealer, rank_only)
         print("Player Lose! Over 21!")
+        return False
 
     else:
-        result(player, dealer)
+        result(player, dealer, rank_only)
         print("Dealer Lose! Over 21!")
+        return True
 
 
-def give_money(win, bj, money, put_down):
-    if win is True and bj is True:
+def give_money(win, money, put_down, player):
+    if win is True and len(player) == 2:
         money = money + put_down * 2
     elif win is True:
         money = money + put_down
     else:
         money = money - put_down
     print("total :", money)
+    return money
 
 
 def is_enough_money(money):
